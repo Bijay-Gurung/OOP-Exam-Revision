89c9ae435e43dc0aa2334570141681696fe2ac7f
Bijay-Gurung <stha4580@gmail.com>
2024-01-05 16:08:20 +0545

    Section2 Completed

--- Revision Tryout/section1/src/oneTryout.java
+++ Revision Tryout/section1/src/oneTryout.java
@@ -1,16 +1,25 @@
-class Animal{
-	void walk() {
-		System.out.println("Animals can walk");
-	}
-}
-class Dog extends Animal{
-	void walk() {
-		System.out.println("Dog can walk");
-	}
-}
-public class oneTryout {
-	public static void main(String[] args) {
-		Dog obj = new Dog();
-		obj.walk();
-	}
-}
+//class Animal{
+//	void walk() {
+//		System.out.println("Animals can walk");
+//	}
+//}
+//class Dog extends Animal{
+//	void walk() {
+//		System.out.println("Dog can walk");
+//	}
+//}
+//public class oneTryout {
+//	public static void main(String[] args) {
+//		Dog obj = new Dog();
+//		obj.walk();
+//	}
+//}
+
+//public class oneTryout{
+//	public static void main(String[] args) {
+//		int[] a = [1,2,3,4,5];
+//		int[] b = a;
+//		b = [];
+//		System.out.println(a);
+//	}
+//}
